18dc5d5fa463e0dbfc33c49a2b4ddb8ab7ad5c6d
Andres Diaz <andresf.diazm@unac.edu.co>
2020-08-07 14:53:03 -0500

    Implementacion del React Router
    Uso de Switch y Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 import Login from './pages/Login/Login';
+import Principal from './pages/Home/Principal';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
 
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Login/>
+        </Route>
+        <Route path="/principal">
+          <Principal/>
+        </Route>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,77 +1,109 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title/Title';
 import Label from './components/Label/Label';
 import Input from './components/Input/Input';
+import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Login.css';
 
-const Login = () =>{
+const Login = ({ navigation }) => {
 
-    const[user,setUser]= useState('');
-    const[password, setPassword]= useState('');
-    const[passwordError, setPasswordError]=useState(false);
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
-    function handleChange(name,value){
-        if(name === 'usuario'){
+    function handleChange(name, value) {
+        if (name === 'usuario') {
             setUser(value)
-        }else{
-            if(value.length<6){
+        } else {
+            if (value.length < 6) {
                 setPasswordError(true);
-            }else{
+            } else {
                 setPasswordError(false);
                 setPassword(value)
             }
-            
+
         }
     }
 
-    function handleSubmit(){
-        let account ={user,password}
-        if(account){
-            console.log('account',account)
+    function ifMatch(param) {
+        if (param.user.length > 0 && param.password.length > 0) {
+            if (param.user === 'Andres' && param.password === "1234567") {
+                let ac = (param.user, param.password);
+                let account = JSON.stringify(ac);
+                localStorage.setItem('account', account);
+                setIsLogin(true);
+                setHasError(false);
+            } else {
+                setIsLogin(false);
+                setHasError(true);
+            }
+        } else {
+            setIsLogin(false);
+            setHasError(true);
         }
     }
-   
-    return(
-        <div className='login-container'>
-        <Title text='Rick and Morty '/>
-        <div className='login-container2'>
-        <Label text='Usuario'/>
-        <Input
-        attribute={{
-            id: 'usuario',
-            name: 'usuario',
-            type: 'text',
-            placeholder:'Ingrese su usuario'
-        }}
-        handleChange={handleChange}
-        />
-        <Label text='Contraseña'/>
-        <Input
-        attribute={{
-            id: 'contraseña',
-            name: 'contraseña',
-            type: 'password',
-            placeholder:'Ingrese su contraseña'
-        }}
-        handleChange={handleChange}
-        param={passwordError}
-        />
-        { passwordError &&
-        <label className='label-error'>
-            Contraseña Invalida o incompleta
-        </label>
+
+    function handleSubmit() {
+        let account = { user, password }
+        if (account) {
+            ifMatch(account);
         }
-        <div>
-        <button onClick={handleSubmit} className='btn btn-primary'>
-            Ingresar
-        </button>
-        </div>
-        </div>
-        
-      
-        </div>
-    )
-};
+    }
+
+    if (hasError === true) {
+        return (
+            <label className='label-alert'>
+                Su contraseña y/o usuario son incorrectos
+            </label>
+        )
+
+    } else {
+
+        return (
+            <div className='login-container'>
+                <Title text='Rick and Morty ' />
+
+                <div className='login-container2'>
+                    <Label text='Usuario' />
+                    <Input
+                        attribute={{
+                            id: 'usuario',
+                            name: 'usuario',
+                            type: 'text',
+                            placeholder: 'Ingrese su usuario'
+                        }}
+                        handleChange={handleChange}
+                    />
+                    <Label text='Contraseña' />
+                    <Input
+                        attribute={{
+                            id: 'contraseña',
+                            name: 'contraseña',
+                            type: 'password',
+                            placeholder: 'Ingrese su contraseña'
+                        }}
+                        handleChange={handleChange}
+                        param={passwordError}
+                    />
+                    {passwordError &&
+                        <label className='label-error'>
+                            Contraseña Invalida o incompleta
+        </label>
+                    }
+                    <div>
+                        <Link to="/principal" onClick={handleSubmit} className="btn btn-dark">
+                            Ingresar
+                    </Link>
+                    </div>
+                </div>
+
+
+            </div>
+        )
+    };
+}
 
 export default Login;
